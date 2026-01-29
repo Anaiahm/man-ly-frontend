89f30e9b7941f84e3c0f206e5d7a03d19c7aa6bb
Anaiah <amaddox.dev@gmail.com>
2026-01-29 14:46:12 -0800

    adjusted background styling for entire app instead of body tag. Changed body tag to div in App.jsx. Used state to connect back end to front end Api message.

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,36 @@
 // import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import HeyMessage from './components/HeyMessage'
 import NavBar from './components/NavBar'
 
 function App() {
   // const [count, setCount] = useState(0)
+    const [apiMessage, setApiMessage] = useState('')
+
+  useEffect(() => {
+    fetch(`${import.meta.env.VITE_API_BASE_URL}/`)
+      .then(res => res.text())
+      .then(data => setApiMessage(data))
+      .catch(err => console.error(err))
+  }, [])
 
   return (
-    <body className="app">
+    <div className="app">
       <NavBar />
       <section className='Hey-Message'>
         <HeyMessage />
       </section>
       <section className='Landing-Section'>
         <h1 className='Landing-Logo'>MAN*LY</h1>
-        <button>
-          Redefining Masculinity through Mental Health, Mentorship, and More
+        <button className='Api-Message'>
+          {apiMessage}
         </button>
       </section>
       <footer className='App-Footer'>
         <p>Dear Men, We love you. -Love, Women</p>
       </footer>
-    </body>
+    </div>
   )
 }
 
---
+++ src/components/LandingNav.tsx
@@ -0,0 +1,13 @@
+import './LandingNav.css';
+
+function LandingNav() {
+  return (
+    <nav className="landing-nav">
+      <img />
+      <img />
+      <img />
+    </nav>
+  );
+};
+
+export default LandingNav;
